b1e06b3add114d127442222164473bac5f85acf9
gustavo <gustavo@brainyit.com.br>
2022-02-27 17:22:17 -0300

    create sidebar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import MainHeader from "./components/main-header";
+import Sidebar from "./components/sidebar";
 
 function App() {
   return (
     <>
+      <Sidebar />
       <MainHeader />
     </>
   );
