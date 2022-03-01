feb0f771578157b0a64e1967ca42e10704da2ddb
gustavo <gustavo@brainyit.com.br>
2022-03-01 00:08:11 -0300

    create container table travel

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -80,7 +80,31 @@ const Home = () => {
               <div className="col-lg-12 col-12">
                 <div class="card card-gray direct-chat-gray">
                   <div class="card-header">
-                    <h3 class="card-title">Maps actions</h3>
+                    <h3 class="card-title">Map actions</h3>
+                    <div class="card-tools">
+                      <button
+                        type="button"
+                        class="btn btn-tool"
+                        data-card-widget="collapse"
+                      >
+                        <i class="fas fa-minus"></i>
+                      </button>
+                    </div>
+                  </div>
+                  <div class="card-body">
+                    <div class="direct-chat-messages"></div>
+                  </div>
+                  <div class="card-footer"></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-lg-12 col-12">
+                <div class="card card-warning direct-chat-warning">
+                  <div class="card-header">
+                    <h3 class="card-title">Table travel</h3>
                     <div class="card-tools">
                       <button
                         type="button"
