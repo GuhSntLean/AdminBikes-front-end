7941eb5c27801fdfb940e6374f309521745d401c
GuhSntLean <guleandro.66@gmail.com>
2022-03-11 00:35:34 -0300

    create private router nothing configuration

---
+++ src/routes/PrivateRouter.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Router, Redirect } from "react-router-dom";
+
+const isAuthenticate = () => {};
+
+const PrivateRouter = ({ ...rest }) => {
+  return <></>;
+};
+
+export default PrivateRouter;
