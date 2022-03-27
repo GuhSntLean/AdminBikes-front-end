c7992db9501f043e28eff7bbefec97b70a263a3a
GuhSntLean <guleandro.66@gmail.com>
2022-03-27 15:42:00 -0300

    adding center container

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,54 +1,63 @@
+import LogoSystem from "../../assets/Logo/LogoSystem.png";
+
 const LoginPage = () => {
   return (
     <>
-      <div class="login-box">
-        <div class="card card-outline card-primary">
-          <div class="card-header text-center">
-            <a href="../../index2.html" class="h1">
-              Gerenciador <b>BikeIot</b>
-            </a>
-          </div>
-          <div class="card-body">
-            <p class="login-box-msg">Faça login pra iniciar sua sessão</p>
-            <form action="../../index3.html" method="post">
-              <div class="input-group mb-3">
-                <input type="email" class="form-control" placeholder="Email" />
-                <div class="input-group-append">
-                  <div class="input-group-text">
-                    <span class="fas fa-envelope"></span>
+      <div class="hold-transition login-page">
+        <div class="login-box">
+          <div class="card">
+            <div class="card-header text-center text-white bg-secondary mb-3 ">
+              <a href="/login" class="h4">
+                <img src={LogoSystem} alt="BikeIoT" className="brand-image m-2" />  
+                Gerenciador <b>BikeIot</b>
+              </a>
+            </div>
+            <div class="card-body">
+              <p class="login-box-msg">Faça login pra iniciar sua sessão</p>
+              <form action="../../index3.html" method="post">
+                <div class="input-group mb-3">
+                  <input
+                    type="text"
+                    class="form-control"
+                    placeholder="Username ou Email"
+                  />
+                  <div class="input-group-append">
+                    <div class="input-group-text">
+                      <span class="fas fa-users"></span>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div class="input-group mb-3">
-                <input
-                  type="password"
-                  class="form-control"
-                  placeholder="Password"
-                />
-                <div class="input-group-append">
-                  <div class="input-group-text">
-                    <span class="fas fa-lock"></span>
+                <div class="input-group mb-3">
+                  <input
+                    type="password"
+                    class="form-control"
+                    placeholder="Password"
+                  />
+                  <div class="input-group-append">
+                    <div class="input-group-text">
+                      <span class="fas fa-lock"></span>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div class="row">
-                <div class="col-8">
-                  <div class="icheck-primary">
-                    <input type="checkbox" id="remember" />
-                    <label for="remember"> Lembrar </label>
+                <div class="row">
+                  <div class="col-8">
+                    <div class="icheck-primary">
+                      <input type="checkbox" id="remember" />
+                      <label for="remember"> Lembrar ?</label>
+                    </div>
                   </div>
-                </div>
 
-                <div class="col-4">
-                  <button type="submit" class="btn btn-primary btn-block">
-                    Sign In
-                  </button>
+                  <div class="col-4">
+                    <button type="submit" class="btn btn-primary btn-block">
+                      Entrar
+                    </button>
+                  </div>
                 </div>
-              </div>
-            </form>
-            <p class="mb-1">
-              <a href="forgot-password.html">Esqueceu a senha ?</a>
-            </p>
+              </form>
+              <p class="mb-1">
+                <a href="forgot-password.html">Esqueceu a senha ?</a>
+              </p>
+            </div>
           </div>
         </div>
       </div>
