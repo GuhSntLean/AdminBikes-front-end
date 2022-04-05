278fb0372636347f25ec52534339b2cc499cbf10
GuhSntLean <guleandro.66@gmail.com>
2022-04-04 21:24:16 -0300

    ajust inital screen

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import MainHeader from "./components/main-header";
+import Sidebar from "./components/sidebar";
 import Home from "./pages/home";
 // import LoginPage from "./pages/login";
 
 function App() {
   return (
     <>
+      <Sidebar />
+      <MainHeader />
       <Home />
     </>
   );
